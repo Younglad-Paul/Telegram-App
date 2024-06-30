d867f94e655d0720ecf1dc9806c79d470c567e33
Borisade Dayo Paul <borisadedayopaul@gmail.com>
2024-06-30 15:54:18 +0100

    creating a telegram mini app

---
+++ src/components/TransferTon.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { Address, toNano, comment } from "ton";
+import { useTonConnect } from "../hooks/useTonConnect";
+import { Card, FlexBoxCol, FlexBoxRow, Button, Input } from "./styled/styled";
+
+export function TransferTon() {
+  const { sender, connected } = useTonConnect();
+
+  const [tonAmount, setTonAmount] = useState("0.01");
+  const [text, setText] = useState("");
+  const [tonRecipient, setTonRecipient] = useState(
+    "EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c"
+  );
+
+  return (
+    <Card>
+      <FlexBoxCol>
+        <h3>Transfer TON</h3>
+        <FlexBoxRow>
+          <label>Amount </label>
+          <Input
+            style={{ marginRight: 8 }}
+            type="number"
+            value={tonAmount}
+            onChange={(e) => setTonAmount(e.target.value)}
+          ></Input>
+        </FlexBoxRow>
+        <FlexBoxRow>
+          <label>To </label>
+          <Input
+            style={{ marginRight: 8 }}
+            value={tonRecipient}
+            onChange={(e) => setTonRecipient(e.target.value)}
+          ></Input>
+        </FlexBoxRow>
+        <FlexBoxRow>
+          <label>Comment </label>
+          <Input
+            style={{ marginRight: 8 }}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          ></Input>
+        </FlexBoxRow>
+        <Button
+          disabled={!connected}
+          style={{ marginTop: 18 }}
+          onClick={async () => {
+            sender.send({
+              to: Address.parse(tonRecipient),
+              value: toNano(tonAmount),
+              body: comment(text),
+            });
+          }}
+        >
+          Transfer
+        </Button>
+      </FlexBoxCol>
+    </Card>
+  );
+}
